1f88e10d3c2248920688292de9e00b56c7e627ea
dwyfrequency <jdwy215@gmail.com>
2019-05-03 12:19:23 -0400

    changed structure of tests

--- Test-Driven-Katas/twoSum.spec.js
+++ Test-Driven-Katas/twoSum.spec.js
@@ -6,8 +6,8 @@ describe('twoSum', () => {
     expect(twoSum(['blem'], 4)).deep.equal([]); // need to use deep equal for arrays an objects
     expect(twoSum([1, 2, 1, 1, 7], 5)).deep.equal([]); // need to use deep equal for arrays an objects
   });
-});
 
-it('Returns correct sum', () => {
-  expect(twoSum([1, 2, 1, 3, 7], 5)).deep.equal([2, 3]); // need to use deep equal for arrays an objects
+  it('Returns correct sum', () => {
+    expect(twoSum([1, 2, 1, 3, 7], 5)).deep.equal([2, 3]); // need to use deep equal for arrays an objects
+  });
 });
